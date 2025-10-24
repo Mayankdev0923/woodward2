798846f7a044e50da8e16a63e69f6388a2098abd
Mayankdev0923 <mayankdev0923@gmail.com>
2025-10-24 19:43:22 +0530

    secured env files and reset new credentials

--- woodward2/src/components/Firebase.tsx
+++ woodward2/src/components/Firebase.tsx
@@ -1,16 +1,17 @@
+// lib/firebase.ts (or wherever your firebase config is)
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC516jLZOmfK0qeahzTn0fh_t4-3Ukph4s",
-  authDomain: "woodward2-7dfae.firebaseapp.com",
-  projectId: "woodward2-7dfae",
-  storageBucket: "woodward2-7dfae.firebasestorage.app",
-  messagingSenderId: "719248142633",
-  appId: "1:719248142633:web:d8b5af816dfb36d1c0c738",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { auth, signInWithEmailAndPassword, onAuthStateChanged,firebaseConfig };
+export { auth, signInWithEmailAndPassword, onAuthStateChanged, firebaseConfig };
